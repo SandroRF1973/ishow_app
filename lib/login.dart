d08013259dc021966ac1f7e595c430748e862d6b
SandroRF <sandrorf1973@gmail.com>
2023-05-10 10:01:38 -0300

    Aula - 252

--- lib/login.dart
+++ lib/login.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:ishow_app/input_customizado.dart';
+import 'package:flutter/scheduler.dart' show timeDilation;
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -37,6 +38,8 @@ class _LoginState extends State<Login> with SingleTickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
+    //timeDilation = 8;
+
     return Scaffold(
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
@@ -80,65 +83,78 @@ class _LoginState extends State<Login> with SingleTickerProviderStateMixin {
               padding: const EdgeInsets.only(left: 30, right: 30),
               child: Column(
                 children: [
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(8),
-                        boxShadow: const [
-                          BoxShadow(
-                              color: Colors.grey,
-                              blurRadius: 15,
-                              spreadRadius: 4)
-                        ]),
-                    child: Column(
-                      children: const [
-                        InputCustomizado(
-                          hint: "Email",
-                          obscure: false,
-                          icon: Icon(Icons.person),
-                        ),
-                        InputCustomizado(
-                          hint: "Senha",
-                          obscure: true,
-                          icon: Icon(Icons.lock),
-                        ),
-                      ],
-                    ),
-                  ),
+                  AnimatedBuilder(
+                      animation: _animacaoSize!,
+                      builder: (context, widget) {
+                        return Container(
+                          width: _animacaoSize!.value,
+                          padding: const EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(8),
+                              boxShadow: const [
+                                BoxShadow(
+                                    color: Colors.grey,
+                                    blurRadius: 15,
+                                    spreadRadius: 4)
+                              ]),
+                          child: Column(
+                            children: const [
+                              InputCustomizado(
+                                hint: "Email",
+                                obscure: false,
+                                icon: Icon(Icons.person),
+                              ),
+                              InputCustomizado(
+                                hint: "Senha",
+                                obscure: true,
+                                icon: Icon(Icons.lock),
+                              ),
+                            ],
+                          ),
+                        );
+                      }),
                   const SizedBox(
                     height: 20,
                   ),
-                  InkWell(
-                    onTap: () {},
-                    child: Container(
-                      height: 50,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          gradient: const LinearGradient(colors: [
-                            Color.fromRGBO(255, 100, 127, 1),
-                            Color.fromRGBO(255, 123, 145, 1)
-                          ])),
-                      child: const Center(
-                        child: Text(
-                          "Entrar",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                    ),
-                  ),
+                  AnimatedBuilder(
+                      animation: _animacaoSize!,
+                      builder: (context, widget) {
+                        return InkWell(
+                          onTap: () {},
+                          child: Container(
+                            height: 50,
+                            width: _animacaoSize!.value,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                gradient: const LinearGradient(colors: [
+                                  Color.fromRGBO(255, 100, 127, 1),
+                                  Color.fromRGBO(255, 123, 145, 1)
+                                ])),
+                            child: const Center(
+                              child: Text(
+                                "Entrar",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ),
+                        );
+                      }),
                   const SizedBox(
                     height: 10,
                   ),
-                  const Text(
-                    "Esqueci minha senha!",
-                    style: TextStyle(
-                        color: Color.fromRGBO(255, 100, 127, 1),
-                        fontWeight: FontWeight.bold),
-                  )
+                  FadeTransition(
+                    opacity: _animacaoFade!,
+                    child: const Text(
+                      "Esqueci minha senha!",
+                      style: TextStyle(
+                          color: Color.fromRGBO(255, 100, 127, 1),
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
                 ],
               ),
             ),
